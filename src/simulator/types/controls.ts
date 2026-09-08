243c317be63877701d05c36591f586bced0b7b66
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2026-09-08 18:39:12 +0100

    Add control slider types and generic train configuration

---
+++ src/simulator/types/controls.ts
@@ -0,0 +1,7 @@
+/** Current values of the driving controls in the UI. */
+export interface ControlState {
+  /** Acceleration slider value from 0 to 100 percent. */
+  accelerate: number;
+  /** Service-brake slider value from 0 to 100 percent. */
+  brake: number;
+}
